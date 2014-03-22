2f9b40564e1991d79b467d1f9ece060bf9edc253
shihang <study1228.sh@gmail.com>
2014-03-22 18:54:43 +0800

    修改deploy部署脚本，添加了jdbc驱动

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
 mkdir -p $CATALINA_HOME/webapps/jdbc/
 cp -r src/main/webapp/* $CATALINA_HOME/webapps/jdbc/
 cp -r target/classes $CATALINA_HOME/webapps/jdbc/WEB-INF/
+
+mkdir $CATALINA_HOME/webapps/jdbc/WEB-INF/lib/
+cp -r lib/mysql-connector-java-3.1.14-bin.jar $CATALINA_HOME/webapps/jdbc/WEB-INF/lib/
