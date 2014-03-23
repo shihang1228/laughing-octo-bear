60b60fc77442e14e7df03c96eda52b11408252e0
shihang <study1228.sh@gmail.com>
2014-03-23 10:17:48 +0800

    新增mysql数据库

---
+++ sql/mysql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `shihang` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `date_created` datetime NOT NULL,
+  `last_updated` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 PACK_KEYS=0;
