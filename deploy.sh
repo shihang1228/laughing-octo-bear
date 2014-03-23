64d72198412d69014c6d0d00a5af8808fc3b9630
shihang <study1228.sh@gmail.com>
2014-03-23 10:35:52 +0800

    修改一个创建目录的bug

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ mkdir -p $CATALINA_HOME/webapps/jdbc/
 cp -r src/main/webapp/* $CATALINA_HOME/webapps/jdbc/
 cp -r target/classes $CATALINA_HOME/webapps/jdbc/WEB-INF/
 
-mkdir $CATALINA_HOME/webapps/jdbc/WEB-INF/lib/
+mkdir -p $CATALINA_HOME/webapps/jdbc/WEB-INF/lib/
 cp -r lib/mysql-connector-java-3.1.14-bin.jar $CATALINA_HOME/webapps/jdbc/WEB-INF/lib/
