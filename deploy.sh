3dee539a41685b93b77b06acf3535b58d4f130e7
shihang <study1228.sh@gmail.com>
2014-03-25 09:58:21 +0800

    在deploy脚本中添加：删除tomcat目录下的jdbc文件夹

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+rm -r -f $CATALINA_HOME/webapps/jdbc
+
 mkdir -p $CATALINA_HOME/webapps/jdbc/
 cp -r src/main/webapp/* $CATALINA_HOME/webapps/jdbc/
 cp -r target/classes $CATALINA_HOME/webapps/jdbc/WEB-INF/
