8ca3eb6a991a5dc6904e3caefc6414cd7f2df8d8
shihang <study1228.sh@gmail.com>
2014-03-25 10:01:42 +0800

    把resp.getWriter（）抽取成out变量

--- src/main/java/com/bodejidi/Manager.java
+++ src/main/java/com/bodejidi/Manager.java
@@ -17,11 +17,12 @@ public class Manager extends HttpServlet
 {
     public void doGet(HttpServletRequest req,HttpServletResponse resp) throws IOException,ServletException
     {
+        resp.setContentType("text/html;charset=UTF-8");
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
         
-       
+        PrintWriter out = resp.getWriter();
         try
         {
             Class.forName("com.mysql.jdbc.Driver").newInstance();           
@@ -30,12 +31,12 @@ public class Manager extends HttpServlet
         {
             //ignore
         }
+        
         try
         {
             conn = DriverManager.getConnection("jdbc:mysql://localhost/test?"
                                                +"user=root"
                                                +"&password=");
-            resp.setContentType("text/html;charset=utf-8");
             String pid = req.getParameter("id");
             stmt = conn.createStatement();
             String sql = "SELECT * FROM shihang";
@@ -48,50 +49,50 @@ public class Manager extends HttpServlet
             }
             
             Long memberId = (Long)session.getAttribute("memberId");
+            
             if(memberId == null)
             {
-                resp.setContentType("text/html;charset=utf-8");
-                resp.getWriter().println("<html><head><title>登录</title></head><body><form action=\"member\" method=\"POST\">");
-                resp.getWriter().println("<label>UserName:<input type=\"text\" name=\"user_name\"></label>");
-                resp.getWriter().println("<label>Password:<input type=\"password\" name=\"password\"></label>");
-                resp.getWriter().println("<input type=\"submit\" name=\"submit_login\" value=\"login\">");
-                resp.getWriter().println("</form></body></html>");
+                out.println("<html><head><title>登录</title></head><body><form action=\"member\" method=\"POST\">");
+                out.println("<label>UserName:<input type=\"text\" name=\"user_name\"></label>");
+                out.println("<label>Password:<input type=\"password\" name=\"password\"></label>");
+                out.println("<input type=\"submit\" name=\"submit_login\" value=\"login\">");
+                out.println("</form></body></html>");
                 return;
             }
             if (pid == null)
             {
-                resp.getWriter().println("<html><head><title>会员管理</title></head><body><h1>会员列表</h1><table border=\"2\">");
+                out.println("<html><head><title>会员管理</title></head><body><h1>会员列表</h1><table border=\"2\">");
                 System.out.println(sql);
                 rs = stmt.executeQuery(sql);
-                resp.getWriter().println("<tr><th>ID</th><th>Name</th></tr>");
+                out.println("<tr><th>ID</th><th>Name</th></tr>");
                 while(rs.next())
                 {
                     Long id = rs.getLong("ID");
                     String firstName = rs.getString("first_name");
                     String lastName = rs.getString("last_name");
-                    resp.getWriter().println("<tr><td><a href=\"?id=" + id + "\" >"+ id + "</a></td><td>" + firstName + lastName + "</td></tr>");
+                    out.println("<tr><td><a href=\"?id=" + id + "\" >"+ id + "</a></td><td>" + firstName + lastName + "</td></tr>");
            
                 }
-                resp.getWriter().println("</table><a href=\".\">Add Member</a></body></html>");
+                out.println("</table><a href=\".\">Add Member</a></body></html>");
             }
             else
             {
-                resp.getWriter().println("<html><head><title>指定会员</title></head><body><h1>指定会员</h1><form action=\"member\" method=\"POST\">");
+                out.println("<html><head><title>指定会员</title></head><body><h1>指定会员</h1><form action=\"member\" method=\"POST\">");
                 sql = sql + " WHERE ID =" + pid;
                 System.out.println(sql);
                 rs = stmt.executeQuery(sql);
-                resp.getWriter().println("<table border=\"2\"><tr><th>ID</th><th>First Name</th><th>Last Name</th></tr>");
+                out.println("<table border=\"2\"><tr><th>ID</th><th>First Name</th><th>Last Name</th></tr>");
                 
                 rs.next();
                 Long id = rs.getLong("ID");
                 String firstName = rs.getString("first_name");
-                String lastName = rs.getString("last_name");
-                resp.getWriter().println("<tr><td>" + id + "</td><td><input type=\"text\" name=\"first_name\" value=\"" + firstName + "\">" 
+                String lastName = rs.getString("last_name");          
+                out.println("<tr><td>" + id + "</td><td><input type=\"text\" name=\"first_name\" value=\"" + firstName + "\">" 
                                        + "</td><td><input type=\"text\" name=\"last_name\" value=\"" + lastName + "\"></td></tr></table></br>");
-                resp.getWriter().println("<input type=\"submit\" name=\"submit_update\" value=\"update\">");
-                resp.getWriter().println("<input type=\"submit\" name=\"submit_delete\" value=\"delete\">");
-                resp.getWriter().println("<input type=\"hidden\" name=\"hidden_update\" value=\"" + id + "\">");
-                resp.getWriter().println("</form><a href=\".\">Add Member</a></body></html>");
+                out.println("<input type=\"submit\" name=\"submit_update\" value=\"update\">");
+                out.println("<input type=\"submit\" name=\"submit_delete\" value=\"delete\">");
+                out.println("<input type=\"hidden\" name=\"hidden_update\" value=\"" + id + "\">");
+                out.println("</form><a href=\".\">Add Member</a></body></html>");
             }
         } 
         catch(SQLException ex)
@@ -99,7 +100,7 @@ public class Manager extends HttpServlet
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
-            resp.getWriter().println("Error!");
+            out.println("Error!");
         }
         finally
         {
@@ -131,11 +132,14 @@ public class Manager extends HttpServlet
         String firstName = req.getParameter("first_name");
         String lastName = req.getParameter("last_name");
         String paraId = req.getParameter("hidden_update");
+        String paraIntr = req.getParameter("introduce");
         String paraUpdate = req.getParameter("submit_update");
         String paraDelete = req.getParameter("submit_delete");
-        resp.setContentType("text/html;charset=utf-8");
+        resp.setContentType("text/html;charset=UTF-8");
         
         String paraLogin = req.getParameter("submit_login");
+        
+        PrintWriter out = resp.getWriter();
         
         
         
@@ -166,33 +170,33 @@ public class Manager extends HttpServlet
                 {
                     HttpSession session = req.getSession();
                     session.setAttribute("memberId", 0L);
-                    resp.getWriter().println("login success!");
-                    resp.getWriter().println("<a href=\"member\">member list</a>");
-                    resp.getWriter().println("<a href=\"?action=logout\">logout</a>");
+                    out.println("login success!");
+                    out.println("<a href=\"member\">member list</a>");
+                    out.println("<a href=\"?action=logout\">logout</a>");
                     
                 }
                 else
                 {
-                    resp.getWriter().println("login failed");
+                    out.println("login failed");
                 }
                 return;
             }
         
             else if("update".equals(paraUpdate))
-            {
-                String sql = "update shihang set first_name='" + firstName + "' ,last_name='" + lastName +"' where ID=" + paraId;
+            {             
+                String sql = "update shihang set first_name='" + firstName + "' ,last_name='" + lastName + "'where ID=" + paraId;
                 System.out.println("SQL: " + sql);
                 stmt.execute(sql);
-                resp.getWriter().println("Update " + firstName + " " + lastName + " Success!");
-                resp.getWriter().println("<a href=\"member\">Member List</a>");
+                out.println("Update " + firstName + " " + lastName + " Success!");
+                out.println("<a href=\"member\">Member List</a>");
             }
             else if("delete".equals(paraDelete))
             {
                 String sql = "delete from shihang where id=" + paraId;
                 System.out.println("SQL: " + sql);
                 stmt.execute(sql);
-                resp.getWriter().println("delete " + firstName + " " + lastName + " Success!");
-                resp.getWriter().println("<a href=\"member\">Member List</a>");
+                out.println("delete " + firstName + " " + lastName + " Success!");
+                out.println("<a href=\"member\">Member List</a>");
             }
             else
             {
@@ -200,8 +204,8 @@ public class Manager extends HttpServlet
                             + firstName + "','" + lastName +"',now(),now())";
                 System.out.println("SQL: " + sql);
                 stmt.execute(sql);
-                resp.getWriter().println("Add " + firstName + " " + lastName + " Success!");
-                resp.getWriter().println("<a href=\"member\">Member List</a>");
+                out.println("Add " + firstName + " " + lastName + " Success!");
+                out.println("<a href=\"member\">Member List</a>");
             }
             
         }
@@ -210,7 +214,7 @@ public class Manager extends HttpServlet
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
-            resp.getWriter().println("Error!");
+            out.println("Error!");
         }
         finally
         {
