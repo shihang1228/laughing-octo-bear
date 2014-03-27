c8783d1cd8d589d1b2667b507c73fd1bde2a4703
shihang <study1228.sh@gmail.com>
2014-03-27 09:39:12 +0800

    将编译的文件改为*.java

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p target/classes/
-javac -encoding "utf-8" -classpath $CATALINA_HOME/lib/servlet-api.jar -d target/classes/ src/main/java/com/bodejidi/Manager.java
+javac -encoding "utf-8" -classpath $CATALINA_HOME/lib/servlet-api.jar -d target/classes/ src/main/java/com/bodejidi/*.java
