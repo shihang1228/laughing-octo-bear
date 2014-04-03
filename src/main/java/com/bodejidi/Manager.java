729ca77da60169704628dcb430d7cec21f94954e
shihang <study1228.sh@gmail.com>
2014-04-03 17:41:09 +0800

    在list页面和show页面显示登录信息

--- src/main/java/com/bodejidi/Manager.java
+++ src/main/java/com/bodejidi/Manager.java
@@ -190,7 +190,7 @@ public class Manager extends HttpServlet
         try
         {
      
-            out.println("<html><head><title>会员管理</title></head><body><h1>会员列表</h1><table border=\"2\">");
+            out.println("<html><head><title>会员管理</title></head><body>" + showLoginInfo() + "<h1>会员列表</h1><table border=\"2\">");
             out.println("<tr><th>ID</th><th>Name</th></tr>");
            
             for(Member member: findAllMember())
@@ -218,7 +218,7 @@ public class Manager extends HttpServlet
         {
             String pid = req.getParameter(FORM_ID);
             Member member = getMemberById(pid);
-            out.println("<html><head><title>指定会员</title></head><body><h1>指定会员</h1><form action=\"member\" method=\"POST\">");
+            out.println("<html><head><title>指定会员</title></head><body>" + showLoginInfo() + "<h1>指定会员</h1><form action=\"member\" method=\"POST\">");
             out.println("<table border=\"2\"><tr><th>ID</th><th>First Name</th><th>Last Name</th></tr>");
             out.println("<tr><td>" + member.getId() + "</td><td><input type=\"text\" name=\"first_name\" value=\"" + member.getFirstName() + "\">" 
                        + "</td><td><input type=\"text\" name=\"last_name\" value=\"" + member.getLastName() + "\"></td></tr></table></br>");
@@ -304,5 +304,9 @@ public class Manager extends HttpServlet
             conn = null;
         }
         return memberList;
+    }
+    public String showLoginInfo()
+    {
+        return "welcome,admin!";
     }
 }
