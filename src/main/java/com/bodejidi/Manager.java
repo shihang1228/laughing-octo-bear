da95277ad2220a6df41b7b1766b370ea6c06d988
shihang <study1228.sh@gmail.com>
2014-04-04 21:57:51 +0800

    将登录页面抽取成login()方法

--- src/main/java/com/bodejidi/Manager.java
+++ src/main/java/com/bodejidi/Manager.java
@@ -50,11 +50,7 @@ public class Manager extends HttpServlet
             
             if(memberId == null)
             {
-                out.println("<html><head><title>登录</title></head><body><form action=\"member\" method=\"POST\">");
-                out.println("<label>UserName:<input type=\"text\" name=\"user_name\"></label>");
-                out.println("<label>Password:<input type=\"password\" name=\"password\"></label>");
-                out.println("<input type=\"submit\" name=\"action\" value=\"login\">");
-                out.println("</form></body></html>");
+                login(req,resp);
                 return;
             }
             if (req.getParameter(FORM_ID) == null)
@@ -63,7 +59,7 @@ public class Manager extends HttpServlet
             }
             else
             {
-                show(req,resp);
+                show(req,resp); 
             }
     }
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException,ServletException
@@ -282,5 +278,14 @@ public class Manager extends HttpServlet
         {
             //ignore;
         }        
+    }
+    public void login(HttpServletRequest req,HttpServletResponse resp) throws IOException,ServletException
+    {
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head><title>登录</title></head><body><form action=\"member\" method=\"POST\">");
+        out.println("<label>UserName:<input type=\"text\" name=\"user_name\"></label>");
+        out.println("<label>Password:<input type=\"password\" name=\"password\"></label>");
+        out.println("<input type=\"submit\" name=\"action\" value=\"login\">");
+        out.println("</form></body></html>");
     }
 }
