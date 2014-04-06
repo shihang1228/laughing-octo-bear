6fc33a319a343021c91768936c20c3a429df40da
shihang <study1228.sh@gmail.com>
2014-04-06 15:21:20 +0800

    在登录页面form表单中的action修改为硬编码

--- src/main/java/com/bodejidi/AuthServlet.java
+++ src/main/java/com/bodejidi/AuthServlet.java
@@ -67,7 +67,7 @@ public class AuthServlet extends HttpServlet
     public void showLoginPage(HttpServletRequest req,HttpServletResponse resp) throws IOException,ServletException
     {
         PrintWriter out = resp.getWriter();
-        out.println("<html><head><title>登录</title></head><body><form action=\"\" method=\"POST\">");
+        out.println("<html><head><title>登录</title></head><body><form action=\"" + req.getRequestURI() + "\" method=\"POST\">");
         out.println("<label>UserName:<input type=\"text\" name=\"user_name\"></label>");
         out.println("<label>Password:<input type=\"password\" name=\"password\"></label>");
         out.println("<input type=\"submit\" name=\"action\" value=\"login\">");
