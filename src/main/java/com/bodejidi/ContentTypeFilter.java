2c7aad88a37576be41a67819714357932311c368
shihang <study1228.sh@gmail.com>
2014-04-06 10:06:47 +0800

    重构了filter中的ContentType

--- src/main/java/com/bodejidi/ContentTypeFilter.java
+++ src/main/java/com/bodejidi/ContentTypeFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.Filter;
 
 public class ContentTypeFilter implements Filter
 {
+    static final String ContentType = "text/html";
     public void init(FilterConfig filterConfig) throws ServletException
     {
         
@@ -16,8 +17,7 @@ public class ContentTypeFilter implements Filter
     public void doFilter(ServletRequest req,ServletResponse resp,FilterChain chain)
                         throws java.io.IOException, ServletException
     {
-        //req.setContentType("text/html");
-        resp.setContentType("text/html");
+        resp.setContentType(ContentType);
         chain.doFilter(req,resp);
     } 
     public void destroy()
