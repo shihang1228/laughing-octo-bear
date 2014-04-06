1370fb006a3fd19f25c5b4fc0339679c34ee6387
shihang <study1228.sh@gmail.com>
2014-04-06 11:50:43 +0800

    如果未登录就显示登录页面，否则跳转到会员管理页面

--- src/main/java/com/bodejidi/AuthServlet.java
+++ src/main/java/com/bodejidi/AuthServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -11,15 +12,28 @@ public class AuthServlet extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
     {
-        showLoginPage(req,resp);
+        if(isNotLogin(req))
+        {
+            showLoginPage(req,resp);
+        }
+        else
+        {
+            resp.sendRedirect("/jdbc");
+        }
     }
     public void showLoginPage(HttpServletRequest req,HttpServletResponse resp) throws IOException,ServletException
     {
         PrintWriter out = resp.getWriter();
-        out.println("<html><head><title>登录</title></head><body><form action=\"member\" method=\"POST\">");
+        out.println("<html><head><title>登录</title></head><body><form action=\"../member\" method=\"POST\">");
         out.println("<label>UserName:<input type=\"text\" name=\"user_name\"></label>");
         out.println("<label>Password:<input type=\"password\" name=\"password\"></label>");
         out.println("<input type=\"submit\" name=\"action\" value=\"login\">");
         out.println("</form></body></html>");
+    }
+     public boolean isNotLogin(HttpServletRequest req) throws ServletException
+    { 
+        HttpSession session = req.getSession();
+        Long memberId = (Long)session.getAttribute("memberId");
+        return null == memberId;
     }
 }
