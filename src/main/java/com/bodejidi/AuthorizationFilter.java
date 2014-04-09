53496a13e4cd3a3592d3863f5dbdd8e8a021f4a7
shihang <study1228.sh@gmail.com>
2014-04-09 14:57:59 +0800

    将Authorization重命名为AuthorizationFilter

---
+++ src/main/java/com/bodejidi/AuthorizationFilter.java
@@ -0,0 +1,41 @@
+package com.bodejidi;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.FilterConfig;
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthorizationFilter implements Filter
+{
+    public void init(FilterConfig filterConfig) throws ServletException
+    {
+        
+    }
+    
+    public void doFilter(ServletRequest request, ServletResponse resp, FilterChain chain)
+    throws java.io.IOException, ServletException
+    {
+        HttpServletRequest req = (HttpServletRequest)request;
+        if(!req.getRequestURI().startsWith(req.getContextPath()+"/auth/") && isNotLogin(req))
+        {
+            ((HttpServletResponse)resp).sendRedirect(req.getContextPath()+"/auth/login");
+            return;
+        }
+        chain.doFilter(req,resp);
+    }
+    public void destroy()
+    {
+        
+    }
+    public boolean isNotLogin(HttpServletRequest req)
+    {
+        HttpSession session = req.getSession();
+        Long memberId = (Long)session.getAttribute("memberId");
+        return memberId == null;
+    }
+}
