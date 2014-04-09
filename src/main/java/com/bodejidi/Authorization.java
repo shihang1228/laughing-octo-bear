c6bd1c14ba6f8947c05ef2e2880580ed13023035
shihang <study1228.sh@gmail.com>
2014-04-09 14:50:58 +0800

    对于非/auth/开头的URL要求登录

--- src/main/java/com/bodejidi/Authorization.java
+++ src/main/java/com/bodejidi/Authorization.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletResponse;
 import javax.servlet.ServletException;
 import javax.servlet.FilterConfig;
 import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class Authorization implements Filter
 {
@@ -13,13 +16,26 @@ public class Authorization implements Filter
     {
         
     }
-    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+    
+    public void doFilter(ServletRequest request, ServletResponse resp, FilterChain chain)
     throws java.io.IOException, ServletException
     {
+        HttpServletRequest req = (HttpServletRequest)request;
+        if(!req.getRequestURI().startsWith(req.getContextPath()+"/auth/") && isNotLogin(req))
+        {
+            ((HttpServletResponse)resp).sendRedirect(req.getContextPath()+"/auth/login");
+            return;
+        }
         chain.doFilter(req,resp);
     }
     public void destroy()
     {
         
+    }
+    public boolean isNotLogin(HttpServletRequest req)
+    {
+        HttpSession session = req.getSession();
+        Long memberId = (Long)session.getAttribute("memberId");
+        return memberId == null;
     }
 }
