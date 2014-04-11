29b1369cbb28ccc149d2e1ddb9338e98a8f395b6
shihang <study1228.sh@gmail.com>
2014-04-11 21:59:29 +0800

    把manager中定义的静态常量重构到Constants类中

--- src/main/java/com/bodejidi/Constants.java
+++ src/main/java/com/bodejidi/Constants.java
@@ -2,5 +2,14 @@ package com.bodejidi;
 
 public class Constants
 {
-    
+    static final String FORM_FIRST_NAME = "first_name"; 
+    static final String FORM_LAST_NAME = "last_name";
+    static final String FORM_ID = "id";
+    static final String FORM_ACTION = "action";
+    static final String SHIHANG_FIRST_NAME = "first_name";
+    static final String SHIHANG_LAST_NAME = "last_name";
+    static final String SHIHANG_ID = "ID";
+    static final String SHIHANG_TABLE = "shihang";
+    static final String SHIHANG_DATE_CREATED = "date_created";
+    static final String SHIHANG_LAST_UPDATED = "last_updated";
 }
