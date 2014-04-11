9a455042d7c5a5bcc502cbd64c07f7f22b358527
shihang <study1228.sh@gmail.com>
2014-04-11 23:02:36 +0800

    将findAllMember()方法重构到MemberService类中

--- src/main/java/com/bodejidi/MemberService.java
+++ src/main/java/com/bodejidi/MemberService.java
@@ -1,5 +1,11 @@
 package com.bodejidi;
 
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.Date;
+import java.util.List;
+import java.util.ArrayList;
+
 import static com.bodejidi.Constants.FORM_FIRST_NAME; 
 import static com.bodejidi.Constants.FORM_LAST_NAME;
 import static com.bodejidi.Constants.FORM_ID;
@@ -26,6 +32,41 @@ public class MemberService
                     + SHIHANG_DATE_CREATED + "," + SHIHANG_LAST_UPDATED + ") VALUES('"
                     + firstName + "','" + lastName +"',now(),now())";
         ds.execute(sql);
+        debug(sql);
         return member;
+    }
+    
+    public List<Member> findAllMember() throws SQLException
+    {
+        List<Member> memberList = new ArrayList<Member>();
+        DataBaseService ds = null;
+        
+        try
+        {
+            ds = DataBaseService.newInstance();
+            String sql = "SELECT * FROM " + SHIHANG_TABLE;
+            debug(sql);
+            ResultSet rs = ds.executeQuery(sql);
+            while(rs.next())
+            {
+                Member member = new Member();
+                member.setId(rs.getLong(SHIHANG_ID));
+                member.setFirstName(rs.getString(SHIHANG_FIRST_NAME));
+                member.setLastName(rs.getString(SHIHANG_LAST_NAME));
+                memberList.add(member);
+                
+            }
+        }
+        finally
+        {
+            ds.close();
+        }
+        return memberList;
+    }
+    
+    public void debug(String str)
+    {
+        String className = Manager.class.getName();
+        System.out.println("[DEBUG] " + new Date() + " " + className + " " + str);
     }
 }
