3b9a0c7e09195747c7111ce14b0a16232d1e961b
shihang <study1228.sh@gmail.com>
2014-04-11 11:27:32 +0800

    新增了jstl 1.2包

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ cp -r src/main/webapp/* $CATALINA_HOME/webapps/jdbc/
 cp -r target/classes $CATALINA_HOME/webapps/jdbc/WEB-INF/
 
 mkdir -p $CATALINA_HOME/webapps/jdbc/WEB-INF/lib/
-cp -r lib/mysql-connector-java-3.1.14-bin.jar $CATALINA_HOME/webapps/jdbc/WEB-INF/lib/
+cp -r lib $CATALINA_HOME/webapps/jdbc/WEB-INF/
