0f3751a52492f68e0530de7411180b2d09b1ce57
shihang <study1228.sh@gmail.com>
2014-04-12 15:01:10 +0800

    修改deploy.sh脚本

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,3 @@ cp -r target/classes $CATALINA_HOME/webapps/jdbc/WEB-INF/
 mkdir -p $CATALINA_HOME/webapps/jdbc/WEB-INF/lib/
 cp -r lib $CATALINA_HOME/webapps/jdbc/WEB-INF/
 
-$CATALINA_HOME/bin/shutdown.sh
-$CATALINA_HOME/bin/startup.sh
-start chrome http://localhost:8080/jdbc/
